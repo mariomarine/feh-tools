6b18fdc3801eb640383442950a5465711287a4f4
mariomarine <rwbleo@gmail.com>
2019-12-08 22:58:08 -0700

    Fix Focus and improve random star rolling

--- summon_sim.clj
+++ summon_sim.clj
@@ -1,3 +1,4 @@
+(require '[clojure.string :as str])
 (println "3 Star Chance (%): ")
 (def threes (read-line))
 (def threes (if (= threes "") "36" threes))
@@ -16,23 +17,49 @@
 (def focus (Integer. focus))
 
 (println "Focus colors (red red blue grey): ")
-(def focus_colors (read-line))
+(def focus_color_wheel (read-line))
+(def focus_color_wheel (if (= focus_color_wheel "") "red red blue grey" focus_color_wheel))
+(def focus_color_wheel (str/split focus_color_wheel #" "))
+
 (def orb1 (rand-int 100))
 (def orb2 (rand-int 100))
 (def orb3 (rand-int 100))
 (def orb4 (rand-int 100))
 (def orb5 (rand-int 100))
 
-(defn getOrbStar [orb] (if (< orb threes) "three"
-                       (if (< (- orb threes) fours) "four"
-                       (if (< (- orb threes fours) fives) "five"
-                       "focus"))))
-(def orb1star (getOrbStar orb1))
-(def orb2star (getOrbStar orb2))
-(def orb3star (getOrbStar orb3))
-(def orb4star (getOrbStar orb4))
-(def orb5star (getOrbStar orb5))
+;; Taken from https://stackoverflow.com/questions/14464011/idiomatic-clojure-for-picking-between-random-weighted-choices
+;; Credit to Rich Hickey (solution) and dripnet (stackoverflow answer)
+(defn wrand 
+  "given a vector of slice sizes, returns the index of a slice given a
+  random spin of a roulette wheel with compartments proportional to
+  slices."
+  [slices]
+  (let [total (reduce + slices)
+        r (rand total)]
+    (loop [i 0 sum 0]
+      (if (< r (+ (slices i) sum))
+        i
+        (recur (inc i) (+ (slices i) sum))))))
 
+(defn getOrbStar [] 
+  (def pick (wrand [threes fours fives focus]))
+  (if (= pick 0) "three"
+  (if (= pick 1) "four"
+  (if (= pick 2) "five"
+  "focus"))
+  )
+)
+
+(def orb1star (getOrbStar))
+(def orb2star (getOrbStar))
+(def orb3star (getOrbStar))
+(def orb4star (getOrbStar))
+(def orb5star (getOrbStar))
+
+(def red_focus_numbers 0)
+(def blue_focus_numbers 0)
+(def green_focus_numbers 0)
+(def grey_focus_numbers 0)
 (def red_5_numbers 49)
 (def blue_5_numbers 35)
 (def green_5_numbers 31)
@@ -49,15 +76,23 @@
 (def grey_3_numbers 30)
 (def three_colors (+ red_3_numbers blue_3_numbers green_3_numbers grey_3_numbers))
 
+(doseq [col focus_color_wheel] (if (= col "red") (def red_focus_numbers (inc red_focus_numbers))
+                          (if (= col "blue") (def blue_focus_numbers (inc blue_focus_numbers))
+                          (if (= col "green") (def green_focus_numbers (inc green_focus_numbers))
+                          (if (= col "grey") (def grey_focus_numbers (inc grey_focus_numbers)) "Bad data")))
+                          )
+)
+(def focus_colors (+ red_focus_numbers blue_focus_numbers green_focus_numbers grey_focus_numbers))
 
 (defn getColor [orbStar]
   (def marker (rand-int (if (= orbStar "three") three_colors
                         (if (= orbStar "four") four_colors
                         (if (= orbStar "five") five_colors
-                        (if (= orbStar "focus") 4 "bad_request")))
+                        (if (= orbStar "focus") focus_colors "bad_request")))
                         )
               )
   )
+  (println marker)
   (if (= orbStar "three") (if (< marker red_3_numbers) "Red 3 Star"
                           (if (< (- marker red_3_numbers) blue_3_numbers) "Blue 3 Star"
                           (if (< (- marker red_3_numbers blue_3_numbers) green_3_numbers) "Green 3 Star"
@@ -73,7 +108,11 @@
                           (if (< (- marker red_5_numbers blue_5_numbers) green_5_numbers) "Green 5 Star"
                           (if (< (- marker red_5_numbers blue_5_numbers green_5_numbers) grey_5_numbers) "Green 5 Star")))
                           )
-  (if (= orbStar "focus") "Focus" "Bad Request")))
+  (if (= orbStar "focus") (if (< marker red_focus_numbers) "Red Focus 5 Star"
+                          (if (< (- marker red_focus_numbers) blue_focus_numbers) "Blue Focus 5 Star"
+                          (if (< (- marker red_focus_numbers blue_focus_numbers) green_focus_numbers) "Green Focus 5 Star"
+                          (if (< (- marker red_focus_numbers blue_focus_numbers green_focus_numbers) grey_focus_numbers) "Green Focus 5 Star")))
+                          ) "Bad Request")))
   )
 )
 (println "Orb 1: " (getColor orb1star))
