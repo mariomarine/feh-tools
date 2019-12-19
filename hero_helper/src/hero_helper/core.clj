e866d2e0ba7b3a7bdc2588e3be023a65b57b12a7
mariomarine <rwbleo@gmail.com>
2019-12-19 14:32:33 -0700

    Encapsulate Address

--- hero_helper/src/hero_helper/core.clj
+++ hero_helper/src/hero_helper/core.clj
@@ -2,13 +2,15 @@
   (:gen-class)
   (:import [org.jsoup Jsoup]))
 
-(defn get-brave-roy-passives []
-  (rest (map #(.text (.child % 1)) (.select (.child (.nextElementSibling (.parent (.first (.select (.select (.get (Jsoup/connect "https://feheroes.gamepedia.com/Roy:_Brave_Lion")) ".mw-parser-output") "#Passives")))) 0) "tr")))
+(def ADDRESS "https://feheroes.gamepedia.com/Roy:_Brave_Lion")
+
+(defn get-address-passives []
+  (rest (map #(.text (.child % 1)) (.select (.child (.nextElementSibling (.parent (.first (.select (.select (.get (Jsoup/connect ADDRESS)) ".mw-parser-output") "#Passives")))) 0) "tr")))
 )
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (get-brave-roy-passives))
+  (println (get-address-passives))
 )
 
